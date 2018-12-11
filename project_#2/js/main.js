04aae133d6622ad34adb99054b3f3b8acdb6208d
Sebastian Łuszczek <36766004+sebastianluszczek@users.noreply.github.com>
2018-12-11 23:51:26 +0100

    Add files via upload

---
+++ project_#2/js/main.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    // sidenav initialization
+    const sidenav_inst = M.Sidenav.init(document.querySelectorAll('.sidenav'), {});
+
+});
