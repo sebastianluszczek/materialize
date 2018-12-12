bd4017741d157e89c3bba7976a188f4bcad46611
utkow <luszczek.sebastian@awesome-ind.com>
2018-12-12 12:59:07 +0100

    finished_project

--- project_#2/js/main.js
+++ project_#2/js/main.js
@@ -1,6 +1,20 @@
 document.addEventListener('DOMContentLoaded', function () {
 
     // sidenav initialization
-    const sidenav_inst = M.Sidenav.init(document.querySelectorAll('.sidenav'), {});
+    const sidenav_init = M.Sidenav.init(document.querySelectorAll('.sidenav'), {});
 
+    // slider - carousel
+    var carousel_init = M.Carousel.init(document.querySelectorAll('.carousel'), {
+        fullWidth: true,
+        indicators: true
+    });
+
+    // modal initialization
+    const modal_init = M.Modal.init(document.querySelectorAll('.modal'), {});
+
+    // modal initialization
+    const tabs_init = M.Tabs.init(document.querySelectorAll('.tabs'), {});
+
+    // select input initialization
+    const select_init = M.FormSelect.init(document.querySelectorAll('select'), {});
 });
